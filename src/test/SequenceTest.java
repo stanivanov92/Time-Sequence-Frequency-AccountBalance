cf515cd3c0f91086932c0dfab93daeba04fc0aa4
Stanislav Ivanov <stan_ivanov@icloud.com>
2020-07-08 08:22:01 +0100

    Longest character sequence and Sequence testing

--- src/test/SequenceTest.java
+++ src/test/SequenceTest.java
@@ -1,3 +1,4 @@
+import javafx.util.Pair;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,6 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class SequenceTest {
 
     @Test
-    void longestSequence() {
+    void longestSequenceTest() {
+        Pair<Character, Integer> testPair = new Pair<>('h',3);
+        assertEquals(testPair,Sequence.longestSequence("dghhhmhmx"));
+
+        testPair = new Pair<>('a',1);
+        assertEquals(testPair,Sequence.longestSequence("A"));
+        assertEquals(testPair,Sequence.longestSequence("a"));
+        assertEquals(null,Sequence.longestSequence(null));
+        assertEquals(null,Sequence.longestSequence(""));
+
+        testPair = new Pair<>('k',3);
+        assertEquals(testPair,Sequence.longestSequence("dhkkhhKKKt"));
+
+        testPair = new Pair<>('l',4);
+        assertEquals(testPair,Sequence.longestSequence("aAabbaAcccKaBakCkQaMNaLaLlLlqQqqlqqq"));
+
+        testPair = new Pair<>('b',3);
+        assertEquals(testPair,Sequence.longestSequence("aBbBadddadd"));
     }
+
 }
